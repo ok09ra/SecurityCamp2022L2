6a1a441afb482879a3d32ec2f4a405e3e2df071d
ok09ra <okudasota@gmail.com>
2022-08-08 21:55:05 +0900

    TLWE temp commit

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ https://nindanaoto.github.io
 
 Security cmap ページ
 https://www.ipa.go.jp/jinzai/camp/2022/zenkoku2022_index.html
+
+参考にしている松岡さんのリポジトリ
+https://github.com/virtualsecureplatform/pyFHE/tree/PurePython
--- TLWE.py
+++ TLWE.py
@@ -1,55 +1,62 @@
 import os
+from pydoc import plain
 import sys
 import random
 import numpy as np
 
+#平文はバイナリでやる
 class TLWE():
-    #平文がTorusな場合を実装
-    def __init__(self, plain_text, n, modula_mu, modula_sigma):
+    def __init__(self, plain_text, mu, n, sigma):
         self.plain_text = plain_text
-        self.length = len(self.plain_text)
-        self.modula_mu = modula_mu
-        self.modula_sigma = modula_sigma
+        self.mu = mu
         self.n = n
-
-    def encrypt_basic(self):
-        self.generate_public_key()
-        self.generate_secret_key()
-        self.generate_error(self.modula_mu, self.modula_sigma)
-        self.encrypt_calc()
-        return np.append(self.public_key, self.cipher_text)
-
-    def generate_public_key(self):
+        self.sigma = sigma
+        
+    def exec(self):
+        self.secret_key = self.generate_secret_key(self.n)
+        self.error = self.generate_error(self.n, self.mu, self.sigma)
+        self.cipher_vector = self.encrypt_calc(self.plain_text, self.secret_key, self.error)
+        self.decrypt_text = self.decrypt_calc(self.cipher_vector, self.secret_key)
+        
+    def generate_public_key(self, n):
         random_from_os = random.SystemRandom()
-        self.public_key = [np.uint32(random_from_os.random()) for i in range(self.n)]
+        return [np.float32(random_from_os.random()) for i in range(n)]
 
-    def generate_secret_key(self):
+    def generate_secret_key(self, n):
         random_from_os = random.SystemRandom()
-        self.secret_key = [np.uint32(random_from_os.random()) for i in range()randint(0,1) for i in range(self.n)]
+        return [np.uint32(1) if random_from_os.randint(0,1) > 0.5 else np.uint32(0) for i in range(n)]
 
-    def generate_error(self, mu, sigma):
+    def generate_error(self, n, mu, sigma):
         #モジューラ正規分布
         random_from_os = random.SystemRandom()
-        self.error = np.mod(np.uint32(random_from_os.normalvariate(mu, sigma)), 1)
+        return [np.mod(np.float32(random_from_os.normalvariate(mu, sigma)), 1) for i in range(n)]
 
-    def encrypt_calc(self):
-        self.cipher_text = np.uint32(np.dot(self.public_key, self.secret_key) + self.plain_text + self.error)
+    def encrypt_calc(self, plain_text, secret_key, error):
+        cipher_vector = np.empty((len(plain_text), self.n + 1))
+        for index in range(len(self.plain_text)):
+            public_key = self.generate_public_key(self.n)
+            cipher_text = np.float32(np.dot(public_key, secret_key) + self.mu * (2 * self.plain_text[index] - 1) + error[index])
+            cipher_vector[index] = np.append(public_key, cipher_text)
+        return cipher_vector
 
-
-    def decrypt_calc(self, encrypted_text:
-        return np.uint32(1 + np.sign(np.uint32()))
-
-    def bootstrap(self):
-
-    def decrypt(self):
-
-
+    def decrypt_calc(self, cipher_vector, secret_key):
+        decrypted_text = np.empty((len(self.plain_text)))
+        for index in range(len(self.plain_text)):
+            decrypted_text[index] = np.float32(1 + np.sign(cipher_vector[index][-1] - np.dot(cipher_vector[index][:-1], secret_key))) / 2
+        return decrypted_text
+    
 def main():
-    tlwe = TLWE("asdfghjkl;", 0, 0.1)
+    mu = 2 ** -3
+    n = 635
+    sigma = 2 ** -19
+    plain_text = [0]
+    
+    tlwe = TLWE(plain_text, mu, n, sigma)
     tlwe.exec()
-    print(tlwe.public_key)
-    print(tlwe.secret_key)
-    print(tlwe.error)
+    #print(f"secret key:\n{tlwe.secret_key}")
+    #print(f"error:\n{tlwe.error}")
+    print(f"plain text:\n{tlwe.plain_text}")
+    print(f"decrypt text:\n{tlwe.decrypt_text}")
     
 
 
